3a9701ff863cbd552fdf8a398663a74960e7faee
Swedge <new.email@example.com>
2020-04-22 21:02:37 +0100

    Rainbow formatter refactored

--- tests/columntests.py
+++ tests/columntests.py
@@ -21,7 +21,8 @@ class TMString(str):
         super().__init__()
 
     @staticmethod
-    def _format_alpha_spec(s, fmt_spec):
+    def _format_alpha_spec(fmt_spec):
+        """Parse any alphabetical flags in the fmt_spec."""
         params_list = []
         if 'b' in fmt_spec:
             params_list.append(BOLD)
@@ -32,11 +33,13 @@ class TMString(str):
         return params_list
 
     @staticmethod
-    def _format_number_spec(s, fmt_spec):
+    def _format_number_spec(fmt_spec):
+        """Parse any numeric flags and convert
+        them to the appropriate escape code"""
         return '\033[' + fmt_spec + 'm'
 
     @staticmethod
-    def _format_foreground_colour(s, fmt_spec):
+    def _format_foreground_colour(fmt_spec):
         return '\033[38;5;' + fmt_spec.replace(fg_escape, '') + 'm'
 
     @staticmethod
@@ -44,23 +47,21 @@ class TMString(str):
         return '\033[48;5;' + fmt_spec.replace(bg_escape, '') + 'm'
 
     def __format__(self, fmt_spec=''):
-        printable = ''
+        """Add a flexible format spec that supports any ANSI escape codes
+        including colours."""
         params = []
-        # print('fmt_spec says:', fmt_spec)
         if fmt_spec:
 
             # Remove whitespace
             fmt_spec.replace(' ', '')
             fmts = fmt_spec.split(',')
-            # print(fmts)
+
             parsed_fmts = []
             for fmt in fmts:
-                # if not fmt.isalnum():
-                #     raise ValueError('fmt_spec is not alphanumeric')
 
                 # First format foreground and background colours as needed
                 if re.search(fg_escape, fmt):
-                    params.append(self._format_foreground_colour(printable, fmt))
+                    params.append(self._format_foreground_colour(fmt))
 
                 if re.search(bg_escape, fmt):
                     params.append(self._format_background_colour(fmt))
@@ -76,11 +77,14 @@ class TMString(str):
 
             for fmt in parsed_fmts:
                 if fmt.isnumeric():
-                    params.append(self._format_number_spec(printable, fmt))
+                    params.append(self._format_number_spec(fmt))
                 elif fmt.isalpha():
-                    plist = self._format_alpha_spec(printable, fmt)
+                    plist = self._format_alpha_spec(fmt)
                     for p in plist:
                         params.append(p)
+
+        # Reset the fmt_spec
+        # fmt_spec = ''
         return "".join(
             ("".join(params), self.content, RESET)
         )
