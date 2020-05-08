cec118cb37fc501b07da4209c5d142117b664aca
Swedge <new.email@example.com>
2020-05-08 11:46:46 +0100

    Docstrings added for style and palette modules
    Docstrings added for Palette class and enhancements to customisation

--- timbermafia/palettes.py
+++ timbermafia/palettes.py
@@ -1,3 +1,9 @@
+"""Colour palettes for timbermafia.
+
+Users can generate their own Palette classes and customise them, for use
+within timbermafia's framework or more generally in python logging
+"""
+
 import collections.abc
 import logging
 import timbermafia.utils as utils
@@ -68,6 +74,14 @@ class Palette:
     """
 
     def __init__(self, preset=None, custom=None):
+        """Init from either a preset palette, or a custom palette dict.
+
+        Palettes generated from presets can be easily changed in-place
+        and are recommended.
+
+        If custom palettes are provided they should resemble the dicts
+        for the palettes present in this module.
+        """
         self.palette_dict = {}
 
         # If given a preset get those settings.
@@ -80,7 +94,14 @@ class Palette:
             except ValueError:
                 raise
 
+    ############################################################
+    # Properties and functions to configure the palette.
+    ############################################################
     def update_colours(self, custom):
+        """Update the existing palette settings from a custom palette dict.
+
+        If a given level's settings are not specified it is unaltered.
+        """
         for key in custom:
             if key in self.palette_dict:
                 self.palette_dict[key].update(custom[key])
@@ -88,20 +109,40 @@ class Palette:
                 self.palette_dict[key] = custom[key]
 
     def set_colours(self, custom):
+        """Overwrite existing palette settings from a custom palette dict."""
         self.palette_dict = custom
 
-    def set_colour(self, levelno, **kwargs):
-        d = {}
-        for kw in kwargs:
-            d[kw] = kwargs[kw]
-        self.palette_dict[levelno] = d
+    def set_colour(self, level, fg=None, bg=None, codes=None, overwrite=False):
+        """Set a given level's colour and appearance settings.
 
+        Args:
+            level: The integer corresponding to the requested level, e.g.
+            20, logging.DEBUG etc.
+            fg: The 8-bit foreground ANSI colour code for this level.
+            bg: The 8-bit background ANSI colour code for this level.
+            codes: An iterable of integers indicating ANSI flags to use.
+            overwrite: If True, overwrites any existing config for this level.
+        """
+        d = {}
+        if fg:
+            d['fg'] = fg
+        if bg:
+            d['bg'] = bg
+        if codes:
+            d['codes'] = codes
+        if overwrite:
+            self.palette_dict[level] = d
+        else:
+            self.palette_dict[level].update(d)
+
+    ############################################################
+    # Funcs for use by the timbermafia classes.
+    ############################################################
     def get_ansi_string(self, levelno):
         s = ''
         d = self.palette_dict.get(levelno)
         if not d:
             return s
-
         fg = d.get('fg')
         if fg:
             s += '\033[38;5;' + str(fg) + 'm'
