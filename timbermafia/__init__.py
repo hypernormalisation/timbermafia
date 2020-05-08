37714c02e56640b6b4696403bbd8469f328543f4
Swedge <new.email@example.com>
2020-05-08 05:25:10 +0100

    Docstrings added for palette and style generation functions

--- timbermafia/__init__.py
+++ timbermafia/__init__.py
@@ -1,54 +1,65 @@
 import logging
 import sys
-import timbermafia.formats
-from timbermafia.palettes import PALETTE_DICT, Palette
-from timbermafia.formatters import TimbermafiaFormatter
-from timbermafia.styles import Style, STYLES
+import timbermafia.formatters
+import timbermafia.palettes
+import timbermafia.styles
 import timbermafia.utils as utils
 
 
-def configure_custom_formatter(style, palette):
-    """Simple function to use a Style to create
-    a timbermafia formatter instance."""
-    return TimbermafiaFormatter(
-        style.format,
-        style.datefmt,
-        style.format_style,
-        timbermafia_style=style,
-        palette=palette
-    )
-
-
-def configure_default_formatter(style):
-    """Simple function to use a Style to create
-    a basic logging.Formatter instance."""
-    return logging.Formatter(
-        style.format,
-        style.datefmt,
-        style.format_style
-    )
-
-
+############################################################
+# Functions to generate style/palette objects from presets.
+############################################################
 def generate_default_style():
-    return Style(preset='default')
+    """Returns an instance of the Style class with the default settings."""
+    return timbermafia.styles.Style(preset='default')
 
 
 def generate_style_from_preset(preset):
-    return Style(preset=preset)
+    """Generate a Style object from a preset style.
+
+    Args:
+        preset: A string with the name of a preset style.
+
+    Returns:
+        Instance of the Style class with settings according to the
+        specified preset.
+    """
+    return timbermafia.styles.Style(preset=preset)
 
 
 def generate_palette_from_preset(preset):
-    return Palette(preset=preset)
+    """Generate a Palette object from a preset palette.
+
+    Args:
+        preset: A string with the name of a preset palette.
+
+    Returns:
+        Instance of the Palette class with settings according to the
+        specified preset.
+    """
+    return timbermafia.palettes.Palette(preset=preset)
 
 
+############################################################
+# Functions to show info on available styles/palettes.
+############################################################
 def print_styles():
-    print('- Preset styles:', ', '.join(STYLES))
+    """Print preset styles with their descriptions."""
+    print('- Preset styles:')
+    for style, conf in timbermafia.styles.style_map.items():
+        print(f'{style} -'.rjust(16), conf['description'])
 
 
 def print_palettes():
-    print('- Preset palettes:', ', '.join(PALETTE_DICT))
+    """Print the preset colour palettes."""
+    print('- Preset palettes:', ', '.join(timbermafia.palettes.palette_dict))
 
 
+############################################################
+# Function to have timbermafia configure logging
+# for an application.
+# Analogous to logging.basicConfig
+############################################################
 def basic_config(
         stream=None, filename=None, filemode='a',
         style=None, palette='sensible',
@@ -80,12 +91,11 @@ def basic_config(
                 logger.removeHandler(h)
                 h.close()
 
-
         # If the given style is a Style instance, use it.
         # Else generate a style from the preset, and
         # set the format and datefmt if specified.
         my_style = style
-        if not isinstance(style, Style):
+        if not isinstance(style, timbermafia.styles.Style):
             my_style = generate_style_from_preset(style)
         if format:
             my_style.format = format
@@ -95,17 +105,19 @@ def basic_config(
         # If the given palette is a Palette instance, use it.
         # Else generate a palette from the preset.
         my_palette = palette
-        if not isinstance(palette, Palette):
+        if not isinstance(palette, timbermafia.palettes.Palette):
             my_palette = generate_palette_from_preset(palette)
 
         # Only create formatters and styles as required.
         use_custom_formatter = stream or (filename and not basic_files)
         custom_formatter, default_formatter = None, None
         if use_custom_formatter:
-            custom_formatter = configure_custom_formatter(my_style, my_palette)
+            f = timbermafia.formatters.configure_custom_formatter
+            custom_formatter = f(my_style, my_palette)
         use_default_formatter = filename and basic_files
         if use_default_formatter:
-            default_formatter = configure_default_formatter(my_style)
+            f = timbermafia.formatters.configure_default_formatter
+            default_formatter = f(my_style)
 
         # Add stream handler if specified
         if stream:
@@ -137,6 +149,9 @@ def basic_config(
         logging._releaseLock()
 
 
+############################################################
+# A class that if inherited from will provide a mixin logger
+############################################################
 class Logged:
     """
     Inherit from this class to provide a mixin logger via
@@ -144,6 +159,9 @@ class Logged:
 
     The log name ensures the root logger is in the logger
     hierarchy, and that its handlers can be used.
+
+    Timbermafia can clean up the "root." from the output
+    automatically for visual clarity.
     """
     @property
     def log(self):
