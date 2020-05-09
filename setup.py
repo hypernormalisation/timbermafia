a44bda5a3adf796aedcc8b9638888f54b49104b6
Swedge <new.email@example.com>
2020-05-09 09:17:17 +0100

    notebook fix

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='timbermafia',
-    version='0.0.1',
+    version='0.1.0',
 
-    description="Make python's logging work for you.",
+    description="Package that makes implementing good-looking "
+                "and flexible logging easy.",
 
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -34,6 +35,7 @@ setup(
         # 'Programming Language :: Python :: 3.5',
         # 'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # This field adds keywords for your project which will appear on the
@@ -43,14 +45,9 @@ setup(
 
     python_requires='>=3.3',
     # install_requires=[]
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    # project_urls={  # Optional
-    #     'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-    #     'Funding': 'https://donate.pypi.org',
-    #     'Say Thanks!': 'http://saythanks.io/to/example',
-    #     'Source': 'https://github.com/pypa/sampleproject/',
-    # },
+
+    project_urls={
+        'Source': 'https://github.com/hypernormalisation/timbermafia',
+        'Bug Reports': 'https://github.com/hypernormalisation/timbermafia/issues',
+    },
 )
